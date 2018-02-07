75ba564b535214cd5a56287daacd2bdcf51a92d3
Dat Toan NGUYEN <dattoan.nguyen@student.vinci.be>
2018-02-07 23:22:13 +0100

    FRONT END - Admin + Machine: togge + update

--- front-end/src/app/redux/modules/machine.js
+++ front-end/src/app/redux/modules/machine.js
@@ -10,6 +10,10 @@ import { appConfig }      from '../../config';
 const UPLOAD_REQUEST:                   string = 'UPLOAD_REQUEST';
 const UPLOAD_RECEIVED:                  string = 'UPLOAD_RECEIVED';
 const UPLOAD_ERROR:                     string = 'UPLOAD_ERROR';
+
+const TOGGLE_UPLOAD_ERROR:              string = 'TOGGLE_UPLOAD_ERROR';
+const TOGGLE_UPLOAD_SUCCESS:            string = 'TOGGLE_UPLOAD_SUCCESS';
+
 const GET_ALL_MACHINES:                  string = 'GET_ALL_MACHINES';
 const GET_ALL_MACHINES_RECEIVED:             string = 'GET_ALL_MACHINES_RECEIVED';
 const GET_ALL_MACHINES_ERROR:                string = 'GET_ALL_MACHINES_ERROR';
@@ -21,7 +25,9 @@ const UPDATE_MACHINES:                string = 'UPDATE_MACHINES';
 
 const initialState = {
   file:           null,
-  upload:         false,
+  uploading:      false,
+  uploadError:    false,
+  uploadSuccess:  false,
   machines:       []
 };
 
@@ -32,19 +38,19 @@ export default function (
     return {
       ...state,
       file: action.payload,
-      upload: true
+      uploading: true
     };
   case UPLOAD_RECEIVED:
     return {
       ...state,
       file: null,
-      upload: false
+      uploading: false
     };
   case UPLOAD_ERROR:
     return{
       ...state,
       file: null,
-      upload: false
+      uploading: false
     };
   case UPDATE_MACHINES:
     return{
@@ -63,6 +69,19 @@ export default function (
     return{
       ...state
     };
+
+    case TOGGLE_UPLOAD_ERROR:
+      return {
+        ...state,
+        uploadError: !state.uploadError
+      };
+    case TOGGLE_UPLOAD_SUCCESS:
+      return {
+        ...state,
+        uploadSuccess: !state.uploadSuccess
+      };
+
+
   default:
     return state;
   }
@@ -167,6 +186,18 @@ export function uploadFile(file) {
         options
       }
     });
+  };
+}
+
+export function toggleUploadError() {
+  return {
+    type: TOGGLE_UPLOAD_ERROR
+  };
+}
+
+export function toggleUploadSuccess() {
+  return {
+    type: TOGGLE_UPLOAD_SUCCESS
   };
 }
 
